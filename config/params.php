6d479ce15fb8afe11a0969f6d18c8602329fa83b
Henry Vega <hp.vega@hotmail.com>
2017-06-27 22:52:44 -0500

    Imprenta

--- config/params.php
+++ config/params.php
@@ -1,7 +1,7 @@
 <?php
 
-Yii::setAlias('@LogoHombreDefault', '/images/usuario_hombre.png');
-Yii::setAlias('@LogoMujerDefault', '/images/usuario_mujer.png');
+Yii::setAlias('@LogoHombreDefault', '/imprenta/images/usuario_hombre.png');
+Yii::setAlias('@LogoMujerDefault', '/imprenta/images/usuario_mujer.png');
 
 return [
     'adminEmail' => 'admin@example.com',
