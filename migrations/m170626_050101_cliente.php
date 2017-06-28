02d64d11e1de880bd435dbf59148a7ce4b765bdc
Henry Vega <hp.vega@hotmail.com>
2017-06-27 22:21:26 -0500

    Imprenta

--- migrations/m170626_050101_cliente.php
+++ migrations/m170626_050101_cliente.php
@@ -10,7 +10,7 @@ class m170626_050101_cliente extends \yii\db\Migration
         if ($this->db->driverName === 'mysql') {
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
         }
-        
+
         $this->createTable('cliente', [
             'id' => $this->primaryKey(),
             'desc_cliente' => $this->string(100),
@@ -20,6 +20,7 @@ class m170626_050101_cliente extends \yii\db\Migration
             'num_telf2' => $this->string(20),
             'dir_mail1' => $this->string(40),
             'dir_mail2' => $this->string(40),
+            'razon_social' => $this->string(100),
             'fecha_digitada' => $this->datetime(),
             'fecha_modificada' => $this->datetime(),
             'fecha_eliminada' => $this->datetime(),
@@ -31,9 +32,54 @@ class m170626_050101_cliente extends \yii\db\Migration
             'estado' => $this->smallInteger(1),
             'distrito' => $this->string(40),
             'provincia' => $this->string(40),
+            'departamento' => $this->string(40),
             'referencia' => $this->string(40),
-            ], $tableOptions);
-                
+        ], $tableOptions);
+
+        $this->insert('cliente', [
+            'desc_cliente' => 'Ripley',
+            'dir_fisica' => 'Av. las Begonias Nro. 545',
+            'numero_ruc' => '20337564373',
+            'num_telf1' => '6115700',
+            'num_telf2' => '6115710',
+            'dir_mail1' => 'distribuidora@ripleyperu.com',
+            'estado' => '1',
+            'distrito' => 'San Isidro',
+            'provincia' => 'Lima',
+            'departamento' => 'Lima, Perú',
+            'referencia' => 'Jardin',
+            'razon_social' => 'TIENDAS POR DEPARTAMENTO RIPLEY S.A.',
+        ]);
+
+        $this->insert('cliente', [
+            'desc_cliente' => 'Banco Ripley',
+            'dir_fisica' => 'Av. Paseo de la Republica Nro. 3118',
+            'numero_ruc' => '20259702411',
+            'num_telf1' => '6115700',
+            'num_telf2' => '6115720',
+            'dir_mail1' => 'comercial@rippleyperu.com',
+            'estado' => '1',
+            'distrito' => 'San Isidro',
+            'provincia' => 'Lima',
+            'departamento' => 'Lima, Perú',
+            'referencia' => '(Piso 11)',
+            'razon_social' => 'BANCO RIPLEY PERU S.A.',
+        ]);
+
+        $this->insert('cliente', [
+            'desc_cliente' => 'Xerox Peru',
+            'dir_fisica' => 'Av. Dionisio Derteano Nro. 144 Dpto. B Int. 1001',
+            'numero_ruc' => '20100119065',
+            'num_telf1' => '6166663',
+            'num_telf2' => '6166666',
+            'dir_mail1' => 'comercial@xerox.com',
+            'estado' => '1',
+            'distrito' => 'San Isidro',
+            'provincia' => 'Lima',
+            'departamento' => 'Lima, Perú',
+            'referencia' => 'Santa Ana',
+            'razon_social' => 'XEROX DEL PERU S.A.',
+        ]);
     }
 
     public function down()
--- views/proforma/_update.php
+++ views/proforma/_update.php
@@ -18,78 +18,76 @@ $descripcion = "Actualizar Proforma";
     <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="x_panel">
             <div class="x_content">
-                <div class="container-fluid">
-                    <?php //Pjax::begin(); ?>
-                    <?php $form = ActiveForm::begin(
-                        [
-                            'enableAjaxValidation' => false,
-                            'enableClientValidation' => true,
-                            'method' => 'post',
-                            'options' => [
-                                'class' => 'form-horizontal form-label-left',
-                                'data-pjax' => true,
-                            ],
-                        ]
-                    ); ?>
-                    <span class="section"><?php echo Html::encode($descripcion) ?></span>
-                    <div class="row">
-                        <div class="item form-group">
-                            <div class="col-md-3 col-sm-6 col-xs-12">
-                                <?= $form->field($model, 'num_proforma')->textInput(
-                                    ['maxlength' => 12],
-                                    ['class' => 'form-control col-md-7 col-xs-12']
-                                ) ?>
-                            </div>
+                <?php Pjax::begin(); ?>
+                <?php $form = ActiveForm::begin(
+                    [
+                        'enableAjaxValidation' => false,
+                        'enableClientValidation' => true,
+                        'method' => 'post',
+                        'options' => [
+                            'class' => 'form-horizontal form-label-left',
+                            'data-pjax' => true,
+                        ],
+                    ]
+                ); ?>
+                <span class="section"><?php echo Html::encode($descripcion) ?></span>
+                <div class="row">
+                    <div class="item form-group">
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <?= $form->field($model, 'num_proforma')->textInput(
+                                ['maxlength' => 12],
+                                ['class' => 'form-control col-md-7 col-xs-12']
+                            ) ?>
+                        </div>
 
-                            <div class="col-md-3 col-sm-6 col-xs-12">
-                                <?= $form->field($model, 'fecha_ingreso')->widget(DatePicker::classname(), [
-                                    'options' => ['placeholder' => ''],
-                                    'value' => date('d-M-Y'),
-                                    'type' => DatePicker::TYPE_COMPONENT_PREPEND,
-                                    'pluginOptions' => [
-                                        'autoclose' => true,
-                                        'format' => 'dd-mm-yyyy',
-                                        'todayHighlight' => true,
-                                        'class' => 'form-control col-md-7 col-xs-12',
-                                    ],
-                                ]);
-                                ?>
-                            </div>
-
-                            <div class="col-md-3 col-sm-6 col-xs-12">
-                                <?= $form->field($model, 'fecha_envio')->widget(DatePicker::classname(), [
-                                    'options' => ['placeholder' => ''],
-                                    'value' => date('d-M-Y'),
-                                    'type' => DatePicker::TYPE_COMPONENT_PREPEND,
-                                    'pluginOptions' => [
-                                        'autoclose' => true,
-                                        'format' => 'dd-mm-yyyy',
-                                        'todayHighlight' => true,
-                                        'class' => 'form-control col-md-7 col-xs-12',
-                                    ],
-                                ]);
-                                ?>
-                            </div>
-
-                            <div class="col-md-3 col-sm-6 col-xs-12">
-                                <?= $form->field($model, 'client')->dropDownList($model->getListCliente(), [
-                                    'prompt' => 'Seleccionar Cliente',
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <?= $form->field($model, 'fecha_ingreso')->widget(DatePicker::classname(), [
+                                'options' => ['placeholder' => ''],
+                                'value' => date('d-M-Y'),
+                                'type' => DatePicker::TYPE_COMPONENT_PREPEND,
+                                'pluginOptions' => [
+                                    'autoclose' => true,
+                                    'format' => 'dd-mm-yyyy',
+                                    'todayHighlight' => true,
                                     'class' => 'form-control col-md-7 col-xs-12',
-                                ]) ?>
-                            </div>
-
+                                ],
+                            ]);
+                            ?>
                         </div>
+
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <?= $form->field($model, 'fecha_envio')->widget(DatePicker::classname(), [
+                                'options' => ['placeholder' => ''],
+                                'value' => date('d-M-Y'),
+                                'type' => DatePicker::TYPE_COMPONENT_PREPEND,
+                                'pluginOptions' => [
+                                    'autoclose' => true,
+                                    'format' => 'dd-mm-yyyy',
+                                    'todayHighlight' => true,
+                                    'class' => 'form-control col-md-7 col-xs-12',
+                                ],
+                            ]);
+                            ?>
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 col-xs-12">
+                            <?= $form->field($model, 'client')->dropDownList($model->getListCliente(), [
+                                'prompt' => 'Seleccionar Cliente',
+                                'class' => 'form-control col-md-7 col-xs-12',
+                            ]) ?>
+                        </div>
+
                     </div>
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <?= DynamicRelations::widget([
-                                'title' => 'Lista de Productos:',
-                                'collection' => $model->proformaDetalles,
-                                'viewPath' => '@app/views/proforma-detalle/create.php',
-                                'collectionType' => new ProformaDetalle(),
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <?= DynamicRelations::widget([
+                            'title' => 'Lista de Productos:',
+                            'collection' => $model->proformaDetalles,
+                            'viewPath' => '@app/views/proforma-detalle/create.php',
+                            'collectionType' => new ProformaDetalle(),
 
-                            ]); ?>
-                        </div>
+                        ]); ?>
                     </div>
                 </div>
             </div>
@@ -101,7 +99,7 @@ $descripcion = "Actualizar Proforma";
                 </div>
             </div>
             <?php ActiveForm::end(); ?>
-            <?php //Pjax::end(); ?>
+            <?php Pjax::end(); ?>
         </div>
     </div>
 </div>
